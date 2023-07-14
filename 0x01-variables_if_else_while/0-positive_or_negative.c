15d2fa32310435fe221e1bcf29ea1d8dd263e5b8
DindiVictoria <dindijemima@gmail.com>
2023-07-14 09:10:34 -0700

    CVariables commit

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,21 +1,29 @@
 #include <stdio.h>
 #include <time.h>
 #include <stdlib.h>
-/* more headers goes there */
 
-/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ *
+ * Description: Assigns a random number to the variable n and prints whether
+ *              the number is positive, zero, or negative.
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-	int n;
+    int n;
 
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-	/* your code goes there */
-	if (n>0)
-		printf("The number %d is positive\n",n);
-	else if (n==0)
-		printf("The number %d is zero\n",n);
-	else
-		printf("The number %d is negative\n",n);
-	return (0);
+    srand(time(0));
+    n = rand() - RAND_MAX / 2;
+
+    if (n > 0)
+        printf("%d is positive\n", n);
+    else if (n == 0)
+        printf("%d is zero\n", n);
+    else
+        printf("%d is negative\n", n);
+
+    return (0);
 }
+
