c35f3b45ad350f58339e4bc49a9459c46db585f6
DindiVictoria <dindijemima@gmail.com>
2023-07-19 06:34:10 -0700

    CFunctions commit

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,44 +1,28 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
  * print_sign - Prints the sign of a number
  * @n: The number to be checked
  *
- * Return: 1 if number > 0, 0 if number is zero, -1 if number is less than zero
+ * Return: 1 if n > 0, 0 if n is zero, -1 if n is less than zero
  */
-int (print_sign)
+int print_sign(int n)
 
 {
-	int number;
-	if (number > 0)
+	int print_sign(int n);
+	if (n > 0)
 	{
-		putchar('+');
+		_putchar('+');
 		return (1);
 	}
-	else if (number == 0)
+	else if (n == 0)
 	{
-		putchar('+');
+		_putchar('+');
 		return (0);
 	}
 	else
 	{
-		putchar('-');
+		_putchar('-');
 		return (-1);
 	}
-}
-
-/**
- * main - Entry point of the program
- *
- * Return: Always 0 (Success)
- */
-int main(void)
-
-{	int number;
-	int result = (print_sign);
-	putchar('\n');
-
-	printf("The sign of %d is %d\n", number, result);
-
-	return (0);
 }
