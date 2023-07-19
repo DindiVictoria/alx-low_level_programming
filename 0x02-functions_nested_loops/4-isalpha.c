5e2ae6f6a6eafc842c164d5b1c2d803449401afb
DindiVictoria <dindijemima@gmail.com>
2023-07-19 06:01:19 -0700

    CFunctions commit

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,3 +1,5 @@
+#include "main.h"
+
 /**
  * _isalpha - Checks if a character is alphabetic
  * @c: The character to be checked
@@ -10,14 +12,4 @@ int _isalpha(int c)
 		return (1);
 	else
 		return (0);
-}
-
-/**
- * main - Entry point of the program
- *
- * Return: 0 (Success)
- */
-int main(void)
-{
-	return (0);
 }
