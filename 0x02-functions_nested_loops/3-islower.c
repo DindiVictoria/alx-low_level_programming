ab78c319791ba78fda65cf5b71ceb85ee086ce13
DindiVictoria <dindijemima@gmail.com>
2023-07-19 05:46:58 -0700

    CFunctions commit

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
  * _islower - Checks if a character is lowercase
@@ -12,14 +12,4 @@ int _islower(int c)
 		return (1);
 	else
 		return (0);
-}
-
-/**
- * main - Entry point of the program
- *
- * Return: 0 (Success)
- */
-int main(void)
-{
-	return (0);
 }
