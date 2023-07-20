ed180cae323a651307bdc85d7941f0320db75034
DindiVictoria <dindijemima@gmail.com>
2023-07-20 10:21:31 -0700

    MoreFunctions commit

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - checks for a digit (0 through 9).
+ *
+ * Return: 1 if c is a digit, 0 otherwise
+ */
+int mul(int a, int b)
+{
+	return a * b;
+}
+
