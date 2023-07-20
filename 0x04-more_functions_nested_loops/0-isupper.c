df32607b0cc6eeb14bb08c04668a04ed07b0441e
DindiVictoria <dindijemima@gmail.com>
2023-07-20 10:13:15 -0700

    MoreFunctions commit

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,6 +1,11 @@
 #include "main.h"
 #include <stdio.h>
 
+/**
+ * main - checks for uppercase character.
+ *
+ * Return: 1 if c is uppercase, 0 otherwise.
+ */
 int _isupper(int c)
 {
 	return (c >= 'A' && c <= 'Z');
