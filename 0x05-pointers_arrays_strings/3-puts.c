a852d01d866854d5c218f33b6ab9798906f318e8
DindiVictoria <dindijemima@gmail.com>
2023-07-24 00:37:41 -0700

    Pointer_Array_Strings commit

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _puts - Prints a string followed by a new line to stdout.
+ * @str: Pointer to the input string.
+ */
+void _puts(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		putchar(str[i]);
+		i++;
+	}
+
+	putchar('\n');
+}
