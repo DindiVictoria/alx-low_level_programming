147369404952aa95ee5e19e104334d0a04cfd05c
DindiVictoria <dindijemima@gmail.com>
2023-07-31 13:17:35 -0700

    0x07 commit

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,41 @@
+#include "main.h"
+
+/**
+ * _strspn - Gets the length of a prefix substring
+ * @s: Pointer to the string to search
+ * @accept: Pointer to the string containing the acceptable characters
+ *
+ * Return: The number of bytes in the initial segment of s that consist only of
+ *         bytes from accept
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int count = 0;
+
+	while (*s != '\0')
+	{
+		char *a = accept;
+
+		while (*a != '\0')
+		{
+			char *a = accept;
+
+			while (*a != '\0')
+			{
+				if (*s == *a)
+				{
+					count++;
+					break;
+				}
+				a++;
+			}
+			if (*a == '\0') /* The character in s is not found in accept */
+				break;
+
+			s++;
+		}
+
+		return (count);
+	}
+
+}
