08229c1fb1ef051668997be4ff1ee29541707c8a
DindiVictoria <dindijemima@gmail.com>
2023-08-14 13:12:12 -0700

    Preprocessor commit

---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,14 @@
+#ifndef ABS_MACRO_H
+#define ABS_MACRO_H
+
+/**
+ * ABS - Computes the absolute value of a number x
+ * @x: The number to compute the absolute value of
+ *
+ * Description: This macro computes the absolute value of the number x.
+ *
+ * Return: The absolute value of x.
+ */
+#define ABS(x) ((x) < 0 ? -(x) : (x))
+
+#endif /* ABS_MACRO_H */
