a8259ad9df22438a5a97dc63a2d6d58e7c76de15
DindiVictoria <dindijemima@gmail.com>
2023-08-14 15:27:01 -0700

    CStructures commit

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -4,13 +4,14 @@
 
 struct dog
 {
-    char *name;
-    float age;
-    char *owner;
+	char *name;
+	float age;
+	char *owner;
 };
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 
+typedef struct dog dog_t;
 
 #endif /* DOG_H */
