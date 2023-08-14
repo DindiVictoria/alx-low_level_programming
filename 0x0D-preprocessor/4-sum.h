71107f6f63a8823f7183efcd0c5100d387c2d2ee
DindiVictoria <dindijemima@gmail.com>
2023-08-14 14:01:07 -0700

    preprocessor commit

---
+++ 0x0D-preprocessor/4-sum.h
@@ -0,0 +1,15 @@
+#ifndef SUM_MACRO_H
+#define SUM_MACRO_H
+
+/**
+ * SUM - Computes the sum of two numbers x and y
+ * @x: The first number
+ * @y: The second number
+ *
+ * Description: This macro computes the sum of the numbers x and y.
+ *
+ * Return: The sum of x and y.
+ */
+#define SUM(x, y) ((x) + (y))
+
+#endif /* SUM_MACRO_H */
