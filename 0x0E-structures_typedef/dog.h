0605b5bbd22f36b31f0a2aa2d5e021bdfbe1557b
DindiVictoria <dindijemima@gmail.com>
2023-08-14 14:08:05 -0700

    CStructures commit

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+/**
+ * struct dog - Represents information about a dog
+ * @name: The name of the dog (string)
+ * @age: The age of the dog (float)
+ * @owner: The owner's name (string)
+ *
+ * Description: This structure defines the attributes of a dog, including
+ * its name, age, and owner's name.
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
