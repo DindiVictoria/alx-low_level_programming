09dbfb1f0ceee7a384cc94317852318b5b4f3c78
DindiVictoria <dindijemima@gmail.com>
2023-08-16 14:03:16 -0700

    Function_pointers commit

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,28 @@
+/* 3-op_functions.c */
+
+#include "calc.h"
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+int op_mul(int a, int b)
+{
+	return (a * b );
+}
+
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
